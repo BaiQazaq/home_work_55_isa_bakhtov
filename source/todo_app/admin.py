81b7ec460bbcf68a1d9efcc33b3e1755a6011574
Isa Bakhtov <issab79@icloud.com>
2022-09-20 18:05:55 +0600

    создана модель и запе
    модель создана в models.py, в admin.py зарегистрирована

--- source/todo_app/admin.py
+++ source/todo_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from todo_app.models import Task
+
 # Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "description", "status", "deadline", "created_at")
+    list_filter = ("id", "description", "status", "deadline", "created_at")
+    search_fields = ("description", "status")
+    fields = ("description", "status", "deadline", "created_at", "changed_at")
+    readonly_fields = ("id", "created_at", "changed_at")
+
+admin.site.register(Task, TaskAdmin)
--- source/todo_app/models.py
+++ source/todo_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    description = models.TextField(verbose_name='Описание',max_length=200, null=False, blank=False)
+    status = models.CharField(verbose_name='Статус', max_length=10, null=False, blank=False, default="new")
+    deadline = models.TextField(verbose_name='Дата выполнения', max_length=16, null=False, blank=False, default="")
+    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    changed_at = models.DateTimeField(verbose_name='Дата изменения', auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.author}"
