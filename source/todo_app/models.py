a9e1c1b64e884374b37750c4bc80ce858edd346b
Isa Bakhtov <issab79@icloud.com>
2022-09-22 23:20:30 +0600

    В связи внесением поля title в модели проведена миграция 0003

--- source/todo_app/models.py
+++ source/todo_app/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-    title = models.CharField(verbose_name='Описание',max_length=100, null=False, blank=False)
-    description = models.TextField(verbose_name='Описание',max_length=500, null=True, default=None)
+    title = models.CharField(verbose_name='Описание',max_length=100, null=False, blank=False, default="No title")
+    description = models.TextField(verbose_name='Описание',max_length=500, null=True)
     status = models.CharField(verbose_name='Статус', max_length=10, null=False, blank=False, default="new")
     deadline = models.DateField(verbose_name='Дата выполнения', max_length=16, null=True, default=None)
     created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
